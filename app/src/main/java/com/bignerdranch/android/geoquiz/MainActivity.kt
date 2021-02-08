fe34a542172a1900b9a76fbe209e939928358609
Juan Vera <jlverarosas@cpp.edu>
2021-02-07 22:46:11 -0800

    Challenge 1

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -19,14 +20,22 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this,
+            var toast = Toast.makeText(
+                this,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.show()
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this,
+            var toast = Toast.makeText(
+                this,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT).show()
+                Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
+
+
         }
 
     }
